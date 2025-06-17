e172d8e75be4f08513e4f79d4b8f0ac76fccb27f
Marcel <146003678+marwoz01@users.noreply.github.com>
2025-06-17 15:36:08 +0200

    Update index.js

--- 06 - Pumpkin's Purrfect Meme Picker/index.js
+++ 06 - Pumpkin's Purrfect Meme Picker/index.js
@@ -2,21 +2,46 @@ import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
 function getEmotionsArray(cats){
-    const emotionsArray = []
+    const emotionsArray = []    
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+            if (!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
         }
     }
     return emotionsArray
 }
 
+
 function renderEmotionsRadios(cats){
+        
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`
     }
     emotionRadios.innerHTML = radioItems
 }
