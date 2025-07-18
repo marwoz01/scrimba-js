c0737aaaefb35165dbe8c129fc4cfa1d22f77774
Marcel <marwozniak01@gmail.com>
2025-07-18 15:30:42 +0200

    I made a template for displaying the movie window

--- 09 - Watch List/script.js
+++ 09 - Watch List/script.js
@@ -1,5 +1,7 @@
 const searchBtn = document.getElementById("search-btn");
 const searchInput = document.getElementById("search-input");
+const startScreen = document.getElementById("start-screen");
+const moviesList = document.getElementById("movies-list");
 
 fetch("http://www.omdbapi.com/?apikey=42b78cff&t=Inception&y=2010&plot=full")
   .then((response) => response.json())
@@ -8,5 +10,14 @@ fetch("http://www.omdbapi.com/?apikey=42b78cff&t=Inception&y=2010&plot=full")
 searchBtn.addEventListener("click", searchMovie);
 
 function searchMovie() {
+  displayMovie();
   console.log(searchInput.value);
+}
+
+function displayMovie() {
+  moviesList.innerHTML += `
+    
+  `;
+
+  startScreen.style.display = "none";
 }
