39e1e174975c26b1cefb467b72712f29e2bc28b7
Marcel <marwozniak01@gmail.com>
2025-07-21 10:43:01 +0200

    Dodano funkcję wyszukiwania filmu z wykorzystaniem OMDb API

--- 09 - Watch List/script.js
+++ 09 - Watch List/script.js
@@ -3,21 +3,58 @@ const searchInput = document.getElementById("search-input");
 const startScreen = document.getElementById("start-screen");
 const moviesList = document.getElementById("movies-list");
 
-fetch("http://www.omdbapi.com/?apikey=42b78cff&t=Inception&y=2010&plot=full")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-
 searchBtn.addEventListener("click", searchMovie);
 
 function searchMovie() {
-  displayMovie();
-  console.log(searchInput.value);
+  const query = searchInput.value.trim();
+
+  if (!query) return;
+
+  fetch(`https://www.omdbapi.com/?apikey=42b78cff&s=${query}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.Response === "False") {
+        moviesList.innerHTML = `<p>Film not found.</p>`;
+        return;
+      }
+      displayMoviesList(data.Search);
+    });
+
+  startScreen.style.display = "none";
 }
 
-function displayMovie() {
-  moviesList.innerHTML += `
-    
-  `;
+function displayMoviesList(movies) {
+  moviesList.innerHTML = "";
+
+  movies.forEach((movie) => {
+    fetch(
+      `https://www.omdbapi.com/?apikey=42b78cff&i=${movie.imdbID}&plot=short`
+    )
+      .then((res) => res.json())
+      .then((fullData) => {
+        moviesList.innerHTML += `
+          <div class="movie">
+            <img src="${fullData.Poster}" />
+            <div class="movie-info">
+              <div class="title-rating">
+                <h3>${fullData.Title}</h3>
+                <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+                <span class="rating">${fullData.imdbRating}</span>
+              </div>
+              <div class="details">
+                <span>${fullData.Runtime}</span>
+                <span>${fullData.Genre}</span>
+                <i class="fa-solid fa-circle-plus"></i>
+                <span>Watchlist</span>
+              </div>
+              <p class="description">
+                ${fullData.Plot}
+              </p>
+            </div>
+          </div>
+        `;
+      });
+  });
 
   startScreen.style.display = "none";
 }
